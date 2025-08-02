04370ac638d1fc930d9ff1af5032b71379f4ce09
gcamarenaprog <gcamarenaprog@outlook.com>
2025-08-02 00:58:08 -0600

    - Carrousel module of home store view.

--- public_html/views/store/modules/carousel.php
+++ public_html/views/store/modules/carousel.php
@@ -1,3 +1,17 @@
+<?php
+  /**
+   * -------------------------------------------------------------------------------------------------------------------
+   * Project name:        Store
+   * Project description: Selection process skills assessment.
+   * Version:             1.0.0
+   * File type:           Template file
+   * File description:    Contains the template categories module.
+   * Module:              Template Store
+   * -------------------------------------------------------------------------------------------------------------------
+   */
+
+?>
+
 <!-- Carousel Start -->
 <div class="container-fluid mb-3">
   <div class="row px-xl-5">
@@ -10,32 +24,32 @@
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item position-relative active" style="height: 430px;">
-            <img class="position-absolute w-100 h-100" src="public_html/resources/store/dist/img/carousel-1.jpg" style="object-fit: cover;">
+            <img class="position-absolute w-100 h-100" src="public_html/resources/store/dist/img/carrousel/carrousel-1.jpg" style="object-fit: cover;">
             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
               <div class="p-3" style="max-width: 700px;">
-                <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Men Fashion</h1>
-                <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
-                <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>
+                <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Redes</h1>
+                <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Productos relacionados con las redes, se subdivide en redes cableadas, inalámbricas y equipo de instalación.</p>
+                <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Comprar ahora</a>
               </div>
             </div>
           </div>
           <div class="carousel-item position-relative" style="height: 430px;">
-            <img class="position-absolute w-100 h-100" src="public_html/resources/store/dist/img/carousel-2.jpg" style="object-fit: cover;">
+            <img class="position-absolute w-100 h-100" src="public_html/resources/store/dist/img/carrousel/carrousel-2.jpg" style="object-fit: cover;">
             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
               <div class="p-3" style="max-width: 700px;">
-                <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Women Fashion</h1>
-                <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
-                <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>
+                <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Software</h1>
+                <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Programas y aplicaciones originales para Windows y MacOS.</p>
+                <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Comprar ahora</a>
               </div>
             </div>
           </div>
           <div class="carousel-item position-relative" style="height: 430px;">
-            <img class="position-absolute w-100 h-100" src="public_html/resources/store/dist/img/carousel-3.jpg" style="object-fit: cover;">
+            <img class="position-absolute w-100 h-100" src="public_html/resources/store/dist/img/carrousel/carrousel-3.jpg" style="object-fit: cover;">
             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
               <div class="p-3" style="max-width: 700px;">
-                <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Kids Fashion</h1>
-                <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
-                <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>
+                <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Equipo de cómputo</h1>
+                <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Todos los equipos de cómputo de escritorio, portátiles y estaciones de trabajo.</p>
+                <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Comprar ahora</a>
               </div>
             </div>
           </div>
@@ -44,19 +58,19 @@
     </div>
     <div class="col-lg-4">
       <div class="product-offer mb-30" style="height: 200px;">
-        <img class="img-fluid" src="public_html/resources/store/dist/img/offer-1.jpg" alt="">
+        <img class="img-fluid" src="public_html/resources/store/dist/img/carrousel/offer-1.jpg" alt="">
         <div class="offer-text">
-          <h6 class="text-white text-uppercase">Save 20%</h6>
-          <h3 class="text-white mb-3">Special Offer</h3>
-          <a href="" class="btn btn-primary">Shop Now</a>
+          <h6 class="text-white text-uppercase">Ahorra 20%</h6>
+          <h3 class="text-white mb-3">Oferta Especial</h3>
+          <a href="" class="btn btn-primary">Comprar ahora</a>
         </div>
       </div>
       <div class="product-offer mb-30" style="height: 200px;">
-        <img class="img-fluid" src="public_html/resources/store/dist/img/offer-2.jpg" alt="">
+        <img class="img-fluid" src="public_html/resources/store/dist/img/carrousel/offer-2.jpg" alt="">
         <div class="offer-text">
-          <h6 class="text-white text-uppercase">Save 20%</h6>
-          <h3 class="text-white mb-3">Special Offer</h3>
-          <a href="" class="btn btn-primary">Shop Now</a>
+          <h6 class="text-white text-uppercase">Ahorra 20%</h6>
+          <h3 class="text-white mb-3">Oferta Especial</h3>
+          <a href="" class="btn btn-primary">Comprar ahora</a>
         </div>
       </div>
     </div>
