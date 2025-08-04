14e519a9ea441be4e0f05a49f59451addb3db843
gcamarenaprog <gcamarenaprog@outlook.com>
2025-08-04 05:01:49 -0600

    - The filter module was translated

--- public_html/views/store/views/shop.php
+++ public_html/views/store/views/shop.php
@@ -58,13 +58,13 @@
     <div class="col-lg-3 col-md-4">
       <!-- Price Start -->
       <h5 class="section-title position-relative text-uppercase mb-3"><span
-            class="bg-secondary pr-3">Filter by price</span></h5>
+            class="bg-secondary pr-3">Filtrar por precio</span></h5>
       <div class="bg-light p-4 mb-30">
         <form>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" checked id="price-all">
-            <label class="custom-control-label" for="price-all">All Price</label>
-            <span class="badge border font-weight-normal">1000</span>
+            <label class="custom-control-label" for="price-all">Sin filtro</label>
+            <span class="badge border font-weight-normal"><?php echo $totalResults; ?></span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" id="price-1">
@@ -97,37 +97,37 @@
 
       <!-- Color Start -->
       <h5 class="section-title position-relative text-uppercase mb-3"><span
-            class="bg-secondary pr-3">Filter by color</span></h5>
+            class="bg-secondary pr-3">Filtrar por stock</span></h5>
       <div class="bg-light p-4 mb-30">
         <form>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" checked id="color-all">
-            <label class="custom-control-label" for="price-all">All Color</label>
-            <span class="badge border font-weight-normal">1000</span>
+            <label class="custom-control-label" for="price-all">Sin filtro</label>
+            <span class="badge border font-weight-normal"><?php echo $totalResults; ?></span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" id="color-1">
-            <label class="custom-control-label" for="color-1">Black</label>
+            <label class="custom-control-label" for="color-1">0-500</label>
             <span class="badge border font-weight-normal">150</span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" id="color-2">
-            <label class="custom-control-label" for="color-2">White</label>
+            <label class="custom-control-label" for="color-2">500-1000</label>
             <span class="badge border font-weight-normal">295</span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" id="color-3">
-            <label class="custom-control-label" for="color-3">Red</label>
+            <label class="custom-control-label" for="color-3">1000-2000</label>
             <span class="badge border font-weight-normal">246</span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" id="color-4">
-            <label class="custom-control-label" for="color-4">Blue</label>
-            <span class="badge border font-weight-normal">145</span>
+            <label class="custom-control-label" for="color-4">2000-4000</label>
+            <span class="badge border font-weight-normal">333</span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between">
             <input type="checkbox" class="custom-control-input" id="color-5">
-            <label class="custom-control-label" for="color-5">Green</label>
+            <label class="custom-control-label" for="color-5">Más de 4000</label>
             <span class="badge border font-weight-normal">168</span>
           </div>
         </form>
@@ -136,39 +136,50 @@
 
       <!-- Size Start -->
       <h5 class="section-title position-relative text-uppercase mb-3"><span
-            class="bg-secondary pr-3">Filter by size</span></h5>
+            class="bg-secondary pr-3">Filtrar por calificación</span></h5>
       <div class="bg-light p-4 mb-30">
         <form>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" checked id="size-all">
-            <label class="custom-control-label" for="size-all">All Size</label>
-            <span class="badge border font-weight-normal">1000</span>
+            <label class="custom-control-label" for="size-all">Sin filtro</label>
+            <span class="badge border font-weight-normal"><?php echo $totalResults; ?></span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" id="size-1">
-            <label class="custom-control-label" for="size-1">XS</label>
+            <label class="custom-control-label" for="size-1">
+              <small class="fa fa-star text-primary mr-1"></small>
+            </label>
             <span class="badge border font-weight-normal">150</span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" id="size-2">
-            <label class="custom-control-label" for="size-2">S</label>
+            <label class="custom-control-label" for="size-2">
+              <small class="fa fa-star text-primary mr-1"></small>
+              <small class="fa fa-star text-primary mr-1"></small>
+            </label>
             <span class="badge border font-weight-normal">295</span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" id="size-3">
-            <label class="custom-control-label" for="size-3">M</label>
+            <label class="custom-control-label" for="size-3">
+              <small class="fa fa-star text-primary mr-1"></small>
+              <small class="fa fa-star text-primary mr-1"></small>
+              <small class="fa fa-star text-primary mr-1"></small>
+            </label>
             <span class="badge border font-weight-normal">246</span>
           </div>
           <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
             <input type="checkbox" class="custom-control-input" id="size-4">
-            <label class="custom-control-label" for="size-4">L</label>
+            <label class="custom-control-label" for="size-4">
+              <small class="fa fa-star text-primary mr-1"></small>
+              <small class="fa fa-star text-primary mr-1"></small>
+              <small class="fa fa-star text-primary mr-1"></small>
+              <small class="fa fa-star text-primary mr-1"></small>
+              <small class="fa fa-star text-primary mr-1"></small>
+            </label>
             <span class="badge border font-weight-normal">145</span>
           </div>
-          <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between">
-            <input type="checkbox" class="custom-control-input" id="size-5">
-            <label class="custom-control-label" for="size-5">XL</label>
-            <span class="badge border font-weight-normal">168</span>
-          </div>
+ 
         </form>
       </div>
       <!-- Size End -->
