426f74353947069421cc0eef79dc6f49b3a5f3dc
gcamarenaprog <gcamarenaprog@outlook.com>
2025-08-10 06:56:40 -0600

    - Link to the products in each category for the store's categories.php module.

--- public_html/views/store/modules/categories.php
+++ public_html/views/store/modules/categories.php
@@ -16,7 +16,7 @@
   
   $functionObject = new Functions();
   $categoriesObject = new ProductCategoriesController();
-  $resultCategories = $categoriesObject->getAllParentCategories ();
+  $categoriesList = $categoriesObject->getAllParentCategories ();
   
   $productObject = new ProductController();
   
@@ -27,18 +27,18 @@
   <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3">Categorías</span></h2>
   <div class="row px-xl-5 pb-3">
     
-    <?php  foreach ($resultCategories as $item): ?>
+    <?php  foreach ($categoriesList as $category): ?>
       
-      <?php  $totalProductsOfCategory = $categoriesObject->getTotalProductsOfCategory ($item['product_category_id']); ?>
+      <?php  $totalProductsOfCategory = $categoriesObject->getTotalProductsOfCategory ($category['product_category_id']); ?>
     
     <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-      <a class="text-decoration-none" href="">
+      <a class="text-decoration-none" href="store?category=<?php echo $category['product_category_id'];?>">
         <div class="cat-item d-flex align-items-center mb-4">
           <div class="overflow-hidden" style="width: 100px; height: 100px;">
-            <img class="img-fluid" src="<?php echo $item['product_category_image']; ?>" alt="">
+            <img class="img-fluid" src="<?php echo $category['product_category_image']; ?>" alt="">
           </div>
           <div class="flex-fill pl-3">
-            <h6><?php echo $item['product_category_name']; ?></h6>
+            <h6><?php echo $category['product_category_name']; ?></h6>
             <small class="text-body"><?php echo $totalProductsOfCategory; ?> Productos</small>
           </div>
         </div>
