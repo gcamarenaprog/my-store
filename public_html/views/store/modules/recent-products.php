206cd21723a40db9088f6e9991e53a98612c539b
gcamarenaprog <gcamarenaprog@outlook.com>
2025-08-10 02:43:00 -0600

    - Refactor code on recent-products.php

--- public_html/views/store/modules/recent-products.php
+++ public_html/views/store/modules/recent-products.php
@@ -16,11 +16,15 @@
   require_once (dirname (__DIR__, 4) . '/php/controllers/ProductController.php');
   require_once (dirname (__DIR__, 4) . '/php/controllers/ProductCategoriesController.php');
   
+  # Declaration and initialization of variables
   $objectFunction = new Functions();
   $categoriesObject = new ProductCategoriesController();
   $productObject = new ProductController();
   
-  $allRecentProducts = $productObject->getAllRecentProducts ();
+  $counterOfCards = 0;
+  
+  # Get recent products
+  $recentProducts = $productObject->getRecentProducts ();
 
 ?>
 
@@ -30,54 +34,60 @@
   </h2>
   <div class="row px-xl-5">
     
-    <?php $counter = 0;
-      foreach ($allRecentProducts as $product): ?>
-        
-        <?php
-        
-        # Only print 8 cards
-        if ($counter >= 8) {
-          break;
-        }
-        
-        $productName = $product['product_name'];
-        $productPrice = number_format ($product['product_price'], 2, '.', ',');
-        $productPriceDisscount = $product['product_price'] + $product['product_price'] * 0.05;
-        $productLikes = $product['product_likes'];
-        $productImage = $product['product_image'];
-        $productId = $product['product_id'];
-        $productViews = $product['product_views'];
-        
-        ?>
-        <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-          <div class="product-item bg-light mb-4">
-            <div class="product-img position-relative overflow-hidden">
-              <img class="img-fluid w-100" src="<?php echo $productImage; ?>" alt="">
-              <div class="product-action">
-                <a class="btn btn-outline-dark btn-square" href="#"><i class="fa fa-shopping-cart"></i></a>
-                <a class="btn btn-outline-dark btn-square" href="#"><i class="far fa-heart"></i></a>
-                <a class="btn btn-outline-dark btn-square" href="#"><i class="fa fa-sync-alt"></i></a>
-                <a class="btn btn-outline-dark btn-square" href="#"><i class="fa fa-search"></i></a>
-              </div>
-            </div>
-            <div class="text-center py-4">
-              <a class="h6 text-decoration-none text-truncate" href=""><?php echo $productName; ?></a>
-              <div class="d-flex align-items-center justify-content-center mt-2">
-                <h5>$<?php echo $productPrice; ?></h5>
-                <h6 class="text-muted ml-2">
-                  <del>$<?php echo $productPriceDisscount; ?></del>
-                </h6>
-              </div>
-              <small class="pt-1">(<?php echo $productViews; ?>) Visitas</small>
-              <div class="d-flex align-items-center justify-content-center mb-1">
-                <?php $objectFunction->printStarsWithScore ($productLikes); ?>
-                <small>(<?php echo $productLikes; ?>)</small>
-              </div>
+    <?php foreach ($recentProducts as $product): ?>
+      
+      <?php
+      # Only print 8 cards
+      if ($counterOfCards >= 8) {
+        break;
+      }
+      ?>
+      <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+        <div class="product-item bg-light mb-4">
+
+          <!-- Product image / Start -->
+          <div class="product-img position-relative overflow-hidden">
+            <img class="img-fluid w-100" src="<?php echo $product['product_image']; ?>" alt="">
+            <div class="product-action">
+              <a class="btn btn-outline-dark btn-square" href="#"><i class="fa fa-shopping-cart"></i></a>
+              <a class="btn btn-outline-dark btn-square" href="#"><i class="far fa-heart"></i></a>
+              <a class="btn btn-outline-dark btn-square" href="#"><i class="fa fa-sync-alt"></i></a>
+              <a class="btn btn-outline-dark btn-square" href="#"><i class="fa fa-search"></i></a>
             </div>
           </div>
+          <!-- Product image / End -->
+
+          <!-- Product data / Start -->
+          <div class="text-center py-4">
+
+            <!-- Name -->
+            <a class="h6 text-decoration-none text-truncate" href=""><?php echo $product['product_name']; ?></a>
+
+            <!-- Prices -->
+            <div class="d-flex align-items-center justify-content-center mt-2">
+              <h5>$<?php echo number_format ($product['product_price'], 2, '.', ','); ?></h5>
+              <h6 class="text-muted ml-2">
+                <del>$<?php echo $product['product_price'] + $product['product_price'] * 0.05; ?></del>
+              </h6>
+            </div>
+
+            <!-- Views -->
+            <small class="pt-1">(<?php echo $product['product_views']; ?>) Visitas</small>
+
+            <!-- Likes -->
+            <div class="d-flex align-items-center justify-content-center mb-1">
+              <?php $objectFunction->printStarsWithScore ($product['product_likes']); ?>
+              <small>(<?php echo $product['product_likes']; ?>)</small>
+            </div>
+
+          </div>
+          <!-- Product data / End -->
+
         </div>
-        
-        <?php $counter++; endforeach; ?>
+      </div>
+      
+      <?php $counterOfCards++; ?>
+    <?php endforeach; ?>
 
   </div>
 </div>
