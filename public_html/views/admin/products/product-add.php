bafa171abfa2b4850ecdb8c02d443235d1e4cf5a
gcamarenaprog <gcamarenaprog@outlook.com>
2025-08-13 08:50:46 -0600

    - Change in the path of the js-product-add.js file

--- public_html/views/admin/products/product-add.php
+++ public_html/views/admin/products/product-add.php
@@ -529,7 +529,7 @@
 <script src="public_html/js/js-functions.js"></script>
 
 <!-- Custom view JS Code /-->
-<script src="public_html/js/js-product-add.js"></script>
+<script src="public_html/js/admin/js-product-add.js"></script>
 
 <script>
 </script>
