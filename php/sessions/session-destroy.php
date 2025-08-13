d26a70218f45a3f4655c052aa07ddc958725af5d
gcamarenaprog <gcamarenaprog@outlook.com>
2025-08-13 02:02:29 -0600

    - Refactoring the session files.

--- php/sessions/session-destroy.php
+++ php/sessions/session-destroy.php
@@ -7,16 +7,14 @@
    * File type:           Sessions file
    * File description:    Destroy the session started by the user.
    * Module:              Sessions
+   * Revised:             13-08-2025
    * -------------------------------------------------------------------------------------------------------------------
    */
-  
-  # Required files
-  require_once (dirname (__DIR__, 1) . '/includes/functions.php');
   
   # Session start
   session_start ();
   session_destroy ();
   
   # Redirect site to log in page
-  header ("Location: ../../login.php?ok=you-have-successfully-logged-out");
+  header ('Location: ../../login.php?success=you-have-successfully-logged-out');
   exit();
--- php/sessions/session-new.php
+++ php/sessions/session-new.php
@@ -8,39 +8,37 @@
    * File description:    Check the username and password from the login screen, check if it exists and start a new
    *                      user session
    * Module:              Sessions
+   * Revised:             13-08-2025
    * -------------------------------------------------------------------------------------------------------------------
    */
   
   # Files required
-  require_once (dirname (__DIR__, 2) . '/install/config.php');
   require_once (dirname (__DIR__, 1) . '/includes/functions.php');
   require_once (dirname (__DIR__, 1) . '/controllers/UserController.php');
   
+  # Declaration and initialization of variables
+  $controllerUserObject = new UserController();
+  
   session_start ();
   
-  # Check if is defined username and password on $_POST variable
-  if (isset($_POST['inputUsername']) && isset($_POST['inputPassword'])) {
+  if (isset($_POST['inputUsername']) && isset($_POST['inputPassword'])) { // Check if is defined username and password on $_POST variable
     
     # Clear username and password strings entered from the login form
     $username = Functions::cleanStringFromInput ($_POST['inputUsername']);
     $password = Functions::cleanStringFromInput ($_POST['inputPassword']);
     
-    # If is empty username or password fields
-    if (empty($username) || empty($password)) {
+    if (empty($username) || empty($password)) { // If is empty username or password fields
       
-      # Redirect to log in screen with an error message in the URL
+      # Redirect to the login page
       header ("Location: ../../login.php?error=empty-fields");
       
-    } else { # If are not empty username or password fields
+    } else { // If are not empty username or password fields
       
-      # Prepare and coding password string to md5
+      # Encode the password string in md5
       $password = md5 ($password);
       
-      # Create new object of User type
-      $controllerUserObject = new UserController();
-      
       # Check if there is a record with the username and password entered
-      $dataUser = $controllerUserObject->verifyUserAndPassword ($username, $password);
+      $dataUser = $controllerUserObject->checkIfTheUserAndPasswordExist ($username, $password);
       
       # If exist data on data user variable
       if ($dataUser) {
@@ -49,7 +47,7 @@
         if ($dataUser['user_username'] === $username && $dataUser['user_password'] === $password) {
           
           # Initializes the SESSION variable from the user data obtained from the database
-          Functions::sessionsInitializeSessionVariable ($dataUser);
+          Functions::initializeSessionVariableWithUserData ($dataUser);
           
           # Redirect to admin
           header ("Location: ../../admin");
@@ -62,8 +60,7 @@
         header ('Location: ../../login.php?error=invalid-user-and-password');
       }
     }
-  } else {
-    # Redirect to log in page with an error message
+  } else { // Redirect to the login page
     header ('Location: ../../login.php?error=empty-fields');
   }
   exit();
