ca6699f3ad2cbf6fed98dde58b590e87a4877933
gcamarenaprog <gcamarenaprog@outlook.com>
2025-08-13 08:48:31 -0600

    - The home-breadcrumbs.php file was created to modularize the store's home view.

---
+++ public_html/views/store/home/home-breadcrumbs.php
@@ -0,0 +1,25 @@
+<?php
+  /**
+   * -------------------------------------------------------------------------------------------------------------------
+   * Project name:        Store
+   * Project description: Selection process skills assessment.
+   * Version:             1.0.0
+   * File type:           Template file
+   * File description:    Contains the template breadcrumbs module.
+   * Module:              Template Store
+   * -------------------------------------------------------------------------------------------------------------------
+   */
+
+?>
+
+<!-- Breadcrumb / Start -->
+<div class="container-fluid">
+  <div class="row px-xl-5">
+    <div class="col-12">
+      <nav class="breadcrumb bg-light mb-30">
+        <span class="breadcrumb-item active"><strong>Incio</strong></span>
+      </nav>
+    </div>
+  </div>
+</div>
+<!-- Breadcrumb / End -->
